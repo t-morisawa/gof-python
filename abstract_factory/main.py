cb3230b012dc36cd7a0a1bd43b800c86b82a8b80
Toma Morisawa <13.35.104@gmail.com>
2019-03-07 23:36:29 +0900

    update for sys.argv practice

--- abstract_factory/main.py
+++ abstract_factory/main.py
@@ -1,10 +1,20 @@
+import sys
 from mizutaki_factory import MizutakiFactory
 from hotpot import HotPot
 from ingregients import Pot
 
+def createFactory(string):
+    if string == "キムチ鍋":
+        return MizutakiFactory() # FIXME KimuchiFactory()
+    else:
+        return MizutakiFactory()
+
 if __name__ == '__main__':
     hotpot = HotPot(Pot())
-    factory = MizutakiFactory()
+    if len(sys.argv) > 1:
+        factory = createFactory(sys.argv[1])
+    else:
+        factory = MizutakiFactory()
     hotpot.addSoup(factory.getSoup())
     hotpot.addMain(factory.getMain())
     hotpot.addVegetables(factory.getVegetables())
