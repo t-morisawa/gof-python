7ce77fd923c7c34b27c6bc48422b2a8f0655f3ea
Toma Morisawa <13.35.104@gmail.com>
2019-03-09 15:40:53 +0900

    add TimerSorter

---
+++ bridge/TimerSorter.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+class TimerSorter(Sorter):
+    def timerSorter(self, list_):
+        start = datetime.now()
+        self.sort(obj)
+        end = datetime.now()
+        print("time:"+(end - start))
