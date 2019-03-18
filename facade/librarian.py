2c7f6c15cd4734d95e90587fb5dab5f63d2bcba7
Toma Morisawa <13.35.104@gmail.com>
2019-03-19 00:01:46 +0900

    add librarian

---
+++ facade/librarian.py
@@ -0,0 +1,23 @@
+# 図書委員の中村くん
+from lendinglist import LendingList
+from booklist import BookList
+
+
+class Librarian:
+    def searchBook(self, bookName):
+        # 本を探す
+        book_list = BookList()
+        location = book_list.searchBook(bookName)
+        # 本の場所がnullではない(所蔵してる)とき
+        if location is not None:
+            # 貸出中かチェックする
+            lending_list = LendingList()
+            if (lending_list.check(bookName)):
+                # 貸出中のとき
+                return "貸出中です"
+            else:
+                # 貸出中ではないとき
+                return location
+        # 所蔵してないとき
+        else:
+            return "その本は所蔵していません"
