53182b777655a5c1802773c57c3605725ec7008d
Toma Morisawa <13.35.104@gmail.com>
2019-03-18 23:53:59 +0900

    add files about facade pattern

---
+++ facade/booklist.py
@@ -0,0 +1,7 @@
+# 所蔵本リスト
+class BookList:
+    def searchBook(self, bookName):
+        location = None
+        # 本の名前から探す
+        # あればその場所を、なければnullを返す
+        return location
---
+++ facade/lendinglist.py
@@ -0,0 +1,6 @@
+# 貸出帳
+class LendingList:
+    def check(self, bookName):
+        # 貸出帳をチェックする
+        # 貸出中ならtrue、そうでなければfalseを返す
+        return True
