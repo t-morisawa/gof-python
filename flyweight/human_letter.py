9ac6820042a5e5ecd381c27fd5e377e0471839f9
Toma Morisawa <13.35.104@gmail.com>
2019-03-21 08:46:04 +0900

    add files of flyweight patterrn

---
+++ flyweight/human_letter.py
@@ -0,0 +1,6 @@
+class HumanLetter():
+    def __init__(self, letter):
+        self.letter = letter
+
+    def getLetter(self):
+        return self.letter
---
+++ flyweight/main.py
@@ -0,0 +1,30 @@
+from human_letter import HumanLetter
+
+
+def takeAPhoto(letter):
+    print(letter.getLetter())
+
+
+if __name__ == '__main__':
+    あ = HumanLetter("あ")
+    takeAPhoto(あ)
+    い = HumanLetter("い")
+    takeAPhoto(い)
+    は = HumanLetter("は")
+    takeAPhoto(は)
+    あ2 = HumanLetter("あ")
+    takeAPhoto(あ2)
+    い2 = HumanLetter("い")
+    takeAPhoto(い2)
+    よ = HumanLetter("よ")
+    takeAPhoto(よ)
+    り = HumanLetter("り")
+    takeAPhoto(り)
+    も = HumanLetter("も")
+    takeAPhoto(も)
+    あ3 = HumanLetter("あ")
+    takeAPhoto(あ3)
+    お = HumanLetter("お")
+    takeAPhoto(お)
+    い3 = HumanLetter("い")
+    takeAPhoto(い3)
