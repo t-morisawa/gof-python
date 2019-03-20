c79f637ef31d145ecacaa2824fb9cde363e8e313
Toma Morisawa <13.35.104@gmail.com>
2019-03-20 23:43:41 +0900

    update state class

--- state/state.py
+++ state/state.py
@@ -10,3 +10,23 @@ class State(metaclass=ABCMeta):
     # 寒いときの対策を返すメソッドを定義する
     def getProtectionForCold(self):
         pass
+
+
+class BadMoodState(State):
+    # 朝のあいさつです。機嫌の悪いときは、ぶっきらぼうに応えます。
+    def morningGreet(self):
+        return "おお"
+
+    # 冬の防寒方法です。機嫌の悪いときはももひきをはきます。
+    def getProtectionForCold(self):
+        return "ももひき"
+
+
+class OrdinaryState(State):
+    # 朝のあいさつです。通常は、男らしく応えます。
+    def morningGreet(self):
+        return "おっす！"
+
+    # 冬の防寒方法です。走るようです。
+    def getProtectionForCold(self):
+        return "走る"
