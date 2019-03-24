fd6ae5ee67bda9bb675dfd300c11912675aca830
Toma Morisawa <13.35.104@gmail.com>
2019-03-24 14:45:04 +0900

    update student for use command classes

--- command/student.py
+++ command/student.py
@@ -1,9 +1,16 @@
 from beaker import Beaker
+from command import AddSaltCommand, AddWaterCommand, MakeSaltWaterCommand
 
 
 if __name__ == "__main__":
-    beaker = Beaker(100, 0)
-    beaker.experiment(Beaker.ADD_SALT)
+    add_salt = AddSaltCommand()
+    add_water = AddWaterCommand()
+    make_salt_water = MakeSaltWaterCommand()
 
-    beaker2 = Beaker(0, 10)
-    beaker2.experiment(Beaker.ADD_WATER)
+    add_salt.set_beaker(Beaker(100, 0))
+    add_water.set_beaker(Beaker(0, 10))
+    make_salt_water.set_beaker(Beaker(90, 10))
+
+    add_salt.execute()
+    add_water.execute()
+    make_salt_water.execute()
