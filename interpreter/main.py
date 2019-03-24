95b3c45b182d74e254c8668f6c1633ab7bd05a47
Toma Morisawa <13.35.104@gmail.com>
2019-03-24 16:16:36 +0900

    add interpreter pattern main function using Expression

--- interpreter/main.py
+++ interpreter/main.py
@@ -18,3 +18,19 @@ if __name__ == '__main__':
     ramen4 = Plus(ramen3, soup).execute()
 
     print(ramen4.get_operand_string())
+
+    """
+    Expressionクラスを使ってかく
+    """
+    salt = Ingredient('粉末スープ')
+    noodle = Ingredient('麺')
+    water = Ingredient('お湯')
+    three = Ingredient('3')
+    soup = Ingredient('液体スープ')
+
+    ramen1 = Expression(Plus(salt, noodle))
+    ramen2 = Expression(Plus(ramen1, water))
+    ramen3 = Expression(Wait(three, ramen2))
+    ramen4 = Expression(Plus(ramen3, soup))
+
+    print(ramen4.get_operand_string())
